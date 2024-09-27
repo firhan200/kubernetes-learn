910754e56de477c6dd52c43b167d09a781518cd6
Firhan Faisal <firhan.faisal1995@gmail.com>
2024-09-27 19:36:20 +0700

    first commit

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3901;
+
+app.get('/', (req, res) => {
+  res.send('File has been changes!');
+});
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
